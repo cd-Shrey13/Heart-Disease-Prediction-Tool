ae24c25dd284c0b63aa24be0ad438c8dccade852
ShreyP. <shreyprajapati13@gmail.com>
2025-01-12 15:50:25 +0530

    Remove .env file, update package.json to include react-router-dom, and refactor HeartDiseasePredictionForm to use backend URL from environment variable

--- Heart-Disease-Frontend/src/HeartDiseasePredictionForm.jsx
+++ Heart-Disease-Frontend/src/HeartDiseasePredictionForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const HeartDiseasePredictionForm = () => {
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
   const [formData, setFormData] = useState({
     age: "",
     sex: "",
@@ -26,7 +27,7 @@ const HeartDiseasePredictionForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://heart-disease-prediction-tool.onrender.com/predict', formData);
+      const response = await axios.post(`${backendUrl}`, formData);
       alert(`Prediction: ${response.data.prediction}`);
     } catch (error) {
       console.error("Error submitting the form:", error);
